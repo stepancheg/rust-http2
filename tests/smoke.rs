d0989c8f64afe6d2d81e417c3c4941407486f660
Stepan Koltsov <stepan.koltsov@gmail.com>
2017-07-06 02:33:57 +0300

    Two more useless smoke tests

--- tests/smoke.rs
+++ tests/smoke.rs
@@ -11,6 +11,7 @@ mod test_misc;
 
 use futures::future;
 use futures::future::Future;
+use futures::stream::Stream;
 
 use httpbis::Client;
 use test_misc::*;
@@ -33,4 +34,46 @@ fn smoke() {
     for rr in r {
         assert_eq!(200000 * 5, rr.body.len());
     }
+}
+
+#[test]
+fn parallel_large() {
+    env_logger::init().ok();
+
+    let server = ServerTest::new();
+
+    let client: Client =
+        Client::new_plain("::1", server.port, Default::default()).expect("client");
+
+    let mut futures = Vec::new();
+    for _ in 0..50 {
+        futures.push(client.start_get("/blocks/100000/5", "localhost").collect());
+    }
+
+    let r = future::join_all(futures).wait().expect("wait");
+    for rr in r {
+        assert_eq!(100000 * 5, rr.body.len());
+    }
+}
+
+#[test]
+fn seq_long() {
+    env_logger::init().ok();
+
+    let server = ServerTest::new();
+
+    let client: Client =
+        Client::new_plain("::1", server.port, Default::default()).expect("client");
+
+    let (headers, parts) = client.start_get("/blocks/100000/100", "localhost").0
+        .wait().expect("get");
+
+    assert_eq!(200, headers.status());
+
+    let mut sum_len = 0;
+    for b in parts.filter_data().wait() {
+        sum_len += b.unwrap().len();
+    }
+
+    assert_eq!(100000 * 100, sum_len);
 }
