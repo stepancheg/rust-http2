7d1fd43aefdb83bf62a159ddcd8f98a76088e0ed
Stepan Koltsov <stepan.koltsov@gmail.com>
2018-05-13 04:34:35 +0100

    Server for h2spec

---
+++ h2spec-test/src/bin/h2spec_server.rs
@@ -0,0 +1,15 @@
+extern crate httpbis;
+extern crate env_logger;
+
+use std::thread;
+
+fn main() {
+    env_logger::init();
+
+    let mut server = httpbis::ServerBuilder::new_plain();
+    server.set_port(8888);
+    let _server = server.build().expect("server.build()");
+    loop {
+        thread::park();
+    }
+}
