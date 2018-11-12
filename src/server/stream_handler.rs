10ddc16a5a183b429ec2d4a3ff657b3d442dd7f0
Stepan Koltsov <stepan.koltsov@gmail.com>
2018-11-12 00:46:47 +0000

    Easier to use ServerStreamHandler

--- src/server/stream_handler.rs
+++ src/server/stream_handler.rs
@@ -5,14 +5,16 @@ use result;
 use ErrorCode;
 use Headers;
 
-/// Synchrnous callback of incoming data
+/// Synchronous callback of incoming data
 pub trait ServerStreamHandler: 'static {
     /// DATA frame received
     fn data_frame(&mut self, data: Bytes, end_stream: bool) -> result::Result<()>;
     /// Trailers HEADERS received
     fn trailers(&mut self, trailers: Headers) -> result::Result<()>;
     /// RST_STREAM frame received
-    fn rst(&mut self, error_code: ErrorCode) -> result::Result<()>;
+    fn rst(&mut self, error_code: ErrorCode) -> result::Result<()> {
+        self.error(error::Error::CodeError(error_code))
+    }
     /// Any other error
     fn error(&mut self, error: error::Error) -> result::Result<()>;
 }
