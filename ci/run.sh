e59c0456f5f8c7578c8eeff9564a29446595ab09
Stepan Koltsov <stepan.koltsov@gmail.com>
2019-10-30 01:12:53 +0000

    Test on Windows

--- ci/run.sh
+++ ci/run.sh
@@ -7,8 +7,10 @@ rustc --version
 cargo test -- --test-threads=1
 cargo test --benches
 
-ci/install-h2spec.sh
-export PATH="$PATH:$(pwd)"
-cargo run --manifest-path h2spec-test/Cargo.toml --bin the_test
+if test -z "$WINDOWS"; then
+    ci/install-h2spec.sh
+    export PATH="$PATH:$(pwd)"
+    cargo run --manifest-path h2spec-test/Cargo.toml --bin the_test
+fi
 
 # vim: set ts=4 sw=4 et:
