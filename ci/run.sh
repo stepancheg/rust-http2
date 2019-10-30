8f465d4cfa165e09eaadb44e5f9ec95b726de401
Stepan Koltsov <stepan.koltsov@gmail.com>
2019-10-30 01:09:39 +0000

    Move h2spec install into ci script

--- ci/run.sh
+++ ci/run.sh
@@ -6,6 +6,9 @@ rustc --version
 
 cargo test -- --test-threads=1
 cargo test --benches
+
+ci/install-h2spec.sh
+export PATH="$PATH:$(pwd)"
 cargo run --manifest-path h2spec-test/Cargo.toml --bin the_test
 
 # vim: set ts=4 sw=4 et:
