c9126cd2e9aa24f091aee369e7ea04e094ebf282
Stepan Koltsov <stepan.koltsov@gmail.com>
2020-01-09 02:07:57 +0000

    Execute h2spec test in a separate job

--- ci/run.sh
+++ ci/run.sh
@@ -4,13 +4,13 @@ export RUST_BACKTRACE=1
 
 rustc --version
 
-cargo test -- --test-threads=1
-cargo test --benches
-
-if test -z "$WINDOWS"; then
+if test "$ACTION" = "h2spec"; then
     ci/install-h2spec.sh
     export PATH="$PATH:$(pwd)"
     cargo run --manifest-path h2spec-test/Cargo.toml --bin the_test
+else
+    cargo test -- --test-threads=1
+    cargo test --benches
 fi
 
 # vim: set ts=4 sw=4 et:
