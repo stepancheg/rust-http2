faba1358a037942b0aa98218597b16e246f6926f
Stiopa Koltsov <stepan.koltsov@gmail.com>
2021-02-24 21:30:24 +0000

    Slightly different NDC interface

--- httpbis/src/log_ndc_future.rs
+++ httpbis/src/log_ndc_future.rs
@@ -2,11 +2,22 @@ use futures::task::Poll;
 use futures::Future;
 use log_ndc::Ndc;
 use std::pin::Pin;
+use std::sync::Arc;
 use std::task::Context;
+
+pub(crate) trait ToNdc {
+    fn to_ndc(&self) -> Ndc;
+}
+
+impl ToNdc for Arc<String> {
+    fn to_ndc(&self) -> Ndc {
+        Ndc::Arc(self.clone())
+    }
+}
 
 pub(crate) fn log_ndc_future<N, F>(ndc: N, f: F) -> LogNdcFuture<N, F>
 where
-    N: Into<Ndc> + Clone,
+    N: ToNdc,
     F: Future,
 {
     LogNdcFuture { ndc, f }
@@ -14,7 +25,7 @@ where
 
 pub(crate) struct LogNdcFuture<N, F>
 where
-    N: Into<Ndc>,
+    N: ToNdc,
     F: Future,
 {
     ndc: N,
@@ -23,13 +34,13 @@ where
 
 impl<N, F> Future for LogNdcFuture<N, F>
 where
-    N: Into<Ndc> + Clone,
+    N: ToNdc,
     F: Future,
 {
     type Output = F::Output;
 
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<F::Output> {
-        let _guard = log_ndc::push(self.ndc.clone().into());
+        let _guard = log_ndc::push(self.ndc.to_ndc());
         unsafe { Pin::new_unchecked(&mut self.get_unchecked_mut().f) }.poll(cx)
     }
 }
